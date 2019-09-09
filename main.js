e348e39c2321e642a94ec2f76dcca4c368090da5
gunt <4261629+gunt@users.noreply.github.com>
2019-09-09 14:33:45 -0600

    conditional statement update main.js

--- main.js
+++ main.js
@@ -8,5 +8,19 @@
 // Function, Methods, properties, conditional statement.
 
 function calc() {
-    var a = 
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = (document.querySelector("#operator").value);
+
+    var calculate;
+
+    if (op == "add") {
+        calculate = a + b;
+    } else if (op == "min") {
+        calculate = a - b;
+    } else if (op == "div") {
+        calculate = a - b;
+    } else if (op == "mul") {
+        calculate = a - b
+    }
 }
