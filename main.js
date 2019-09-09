71ec6fcf8de78f7b5749802f8ca20d2285fe4c21
gunt <4261629+gunt@users.noreply.github.com>
2019-09-09 13:54:38 -0600

    Function, Methods, properties, conditional statement.

---
+++ main.js
@@ -0,0 +1,12 @@
+// Inside the function calc (inside curly bracket 
+// we need to do 
+// 3 different things.
+// 1. Grab the value from inside the form. 
+// 2. Then Calcualate them.
+// 3. Output inside the website
+
+// Function, Methods, properties, conditional statement.
+
+function calc() {
+    var a = 
+}
